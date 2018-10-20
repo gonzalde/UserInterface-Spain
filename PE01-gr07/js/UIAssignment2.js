1676b9aefe8748a9b90d030b76c6109237b505e7
tkaizer <tkaizer@u.rochester.edu>
2018-10-20 15:57:29 +0200

    Fixed merge errors and added the non functioning registration code I've written

--- PE01-gr07/js/UIAssignment2.js
+++ PE01-gr07/js/UIAssignment2.js
@@ -58,3 +58,49 @@ function checkPreferences(){
     }
     return true;
 }
+
+//Registration and cookie saving functions
+var today = new Date();
+var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
+
+function saveCookies(){
+    var userName = $('rUserName').val();
+    alert(userName);
+    var password = $('rPassword').val();
+    var first = $('rFirst').val();
+    var last = $('rLast').val();
+    var email = $('rEmail').val();
+    var dob = $('dob').val();
+    var proPic = $('proPic').val();
+    var address = $('address').val();
+
+   $.cookie = ("theName" , [password, first, last, email, dob, proPic, address])
+}
+
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
+
+function checkCookies(){
+    var userName = $.cookie($("theName"));
+    alert(userName);
+    if(userName !== ""){
+        alert("account with this username already exists");
+    }
+    else{
+        saveCookies();
+    }
+}
+
